71fc8b5d3c2a539353a5bbb4a5f54bdea0af4aa4
cendekiapp <cendiqkrn@gmail.com>
2015-06-12 16:39:47 +0700

    add dbal, create local .env, and create migration posts table

---
+++ database/migrations/2015_06_12_092241_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title_locale_en')->nullable();
+			$table->string('title_locale_zh_tw')->nullable();
+			$table->text('description_locale_en')->nullable();
+			$table->text('description_locale_zh_tw')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
